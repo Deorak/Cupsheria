79bfe62e2da92859541473002e37b5cbf3122884
Deorak <pav.bogdan@yahoo.com>
2025-08-26 13:47:15 +0300

    commit bug fix in contact form and add events page

---
+++ test.js
@@ -0,0 +1,12 @@
+function sendMail(){
+  let parms = {
+    name : document.getElementById("message-name").value,
+    email : document.getElementById("message-email").value,
+    telephone : document.getElementById("message-phone").value,
+    message : document.getElementById("message-content").value,
+  }
+
+  emailjs.send("service_86te3q4", "template_c65oecn", parms).then(alert("Email Sent!"))
+}
+ 
+
