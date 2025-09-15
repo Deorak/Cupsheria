30fa681fb366520231f0269984d2615b7ad91d75
Deorak <pav.bogdan@yahoo.com>
2025-09-16 00:46:26 +0300

    css mods

--- test.js
+++ test.js
@@ -1,11 +1,22 @@
-function sendMail(){
+function sendMail(event){
+  console.log("inside function");
+  if (event) event.preventDefault();
   let parms = {
     name : document.getElementById("message-name").value,
     email : document.getElementById("message-email").value,
     telephone : document.getElementById("message-phone").value,
     message : document.getElementById("message-content").value,
+    cart : sessionStorage.getItem("cart") || "empty"
   }
-  emailjs.send("service_86te3q4", "template_c65oecn", parms).then(alert("Email Sent!"))
+  console.log(parms);
+  emailjs.send("service_86te3q4", "template_c65oecn", parms)
+    .then(() => {
+      alert("Email Sent!");
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+      alert("Failed to send email");
+    });
 }
  
 
